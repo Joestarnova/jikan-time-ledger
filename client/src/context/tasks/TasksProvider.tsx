41e0af41fd8232b063fc88ef574b2d252ff334a2
Joestarnova <kbmagno14@gmail.com>
2026-06-28 17:24:43 +0800

    Fixed reloading bug by refactoring TasksProvider and TasksPage to use async/await for loading tasks

--- client/src/context/tasks/TasksProvider.tsx
+++ client/src/context/tasks/TasksProvider.tsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Task } from "../../types";
 import { TasksContext } from "./tasksContext";
+import { api } from "../../lib/api";
 
 export function TasksProvider({ children }: { children: React.ReactNode }) {
   const [tasks, setTasks] = useState<Task[]>([]);
+
+    useEffect(() => {
+    let cancelled = false;
+     
+    (async () => {
+      try {
+        const data = await api.getTasks();
+      if (!cancelled) {
+        setTasks(data);
+      }
+      } catch (err) {
+        console.error("Failed to load tasks:", err);
+      }
+    })()
+
+    return () => {
+      cancelled = true;
+    };
+  }, []);
 
   return (
     <TasksContext.Provider value={{ tasks, setTasks }}>
